0d52a3f0ce6920bf7422c9f8921eaf7b80fa9adb
Jane Doe <gabrieldibuongrazio@gmai.com>
2025-03-12 17:27:49 -0400

    Pregunta3
    3.Lista los 5 sellos discográficos (label) con mayor cantidad de reseñas publicadas.

--- Formulario.R
+++ Formulario.R
@@ -13,3 +13,4 @@ Pregunta1 <-dbGetQuery(portaldb,"SELECT count(reviewid), artist FROM reviews GRO
 
 Pregunta2 <-dbGetQuery(portaldb,"SELECT avg(score) , reviewid, pub_year FROM reviews GROUP by reviewid  HAVING pub_year BETWEEN 2010 and 2014 ORDER by avg(score) DESC ;")
 
+Pregunta3 <-dbGetQuery(portaldb,"SELECT label, count(reviewid) FROM labels GROUP by label ORDER by count(reviewid) desc limit 5;")
