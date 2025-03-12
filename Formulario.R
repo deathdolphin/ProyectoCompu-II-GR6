c7b830f1334938d23134d03b4c50547dc6cb7b49
Jane Doe <gabrieldibuongrazio@gmai.com>
2025-03-12 18:13:18 -0400

    Pregunta4
    4.	¿Cuántos álbumes recibieron la distinción de "Best New Music" en 2016?

--- Formulario.R
+++ Formulario.R
@@ -14,3 +14,6 @@ Pregunta1 <-dbGetQuery(portaldb,"SELECT count(reviewid), artist FROM reviews GRO
 Pregunta2 <-dbGetQuery(portaldb,"SELECT avg(score) , reviewid, pub_year FROM reviews GROUP by reviewid  HAVING pub_year BETWEEN 2010 and 2014 ORDER by avg(score) DESC ;")
 
 Pregunta3 <-dbGetQuery(portaldb,"SELECT label, count(reviewid) FROM labels GROUP by label ORDER by count(reviewid) desc limit 5;")
+
+Pregunta4 <-dbGetQuery(portaldb,"SELECT count(title), title from reviews group by title HAVING  best_new_music=1 and pub_year=2016 ;  ")
+"Esta pregunta tiene la particularidad de que para ver el resultado no se ve los datos del dataframe si no la cantidad de datos del dataframe, eneste caso 90 albumes recibieron el reconociemiento"
