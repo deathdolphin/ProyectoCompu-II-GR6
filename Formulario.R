636e69cd7b30a0c67eceed5c4edc2e5435da68ca
Jane Doe <gabrieldibuongrazio@gmai.com>
2025-03-17 14:00:33 -0400

    Actualización pregunta 2
    Arreglar error, debido a mala interpretación de la pregunta 2

--- Formulario.R
+++ Formulario.R
@@ -9,9 +9,9 @@ portaldb <- dbConnect(SQLite(),"C:/Users/gabri/Downloads/database.sqlite")
 
 dbListTables(portaldb)
 
-Pregunta1 <-dbGetQuery(portaldb,"SELECT count(reviewid), artist FROM reviews GROUP by (artist) ORDER by (count(reviewid)) desc limit 1;")
+Pregunta1 <-dbGetQuery(portaldb,"SELECT count(reviewid), artist FROM reviews GROUP by (artist) ORDER by (count(reviewid)) desc limit 2;")
 
-Pregunta2 <-dbGetQuery(portaldb,"SELECT avg(score) , reviewid, pub_year FROM reviews GROUP by reviewid  HAVING pub_year BETWEEN 2010 and 2014 ORDER by avg(score) DESC ;")
+Pregunta2 <-dbGetQuery(portaldb,"SELECT avg(score) FROM reviews GROUP by pub_year BETWEEN 2010 and 2014  HAVING pub_year BETWEEN 2010 and 2014 ;")
 
 Pregunta3 <-dbGetQuery(portaldb,"SELECT label, count(reviewid) FROM labels GROUP by label ORDER by count(reviewid) desc limit 5;")
 
@@ -19,3 +19,4 @@ Pregunta4 <-dbGetQuery(portaldb,"SELECT count(title), title from reviews group b
 "Esta pregunta tiene la particularidad de que para ver el resultado no se ve los datos del dataframe si no la cantidad de datos del dataframe, eneste caso 90 albumes recibieron el reconociemiento"
 
 Pregunta5 <-dbGetQuery(portaldb," SELECT genre, AVG(reviews.score) AS average_score FROM genres JOIN reviews ON reviews.reviewid = genres.reviewid group by genre ORDER BY average_score DESC LIMIT 3; ")
+
